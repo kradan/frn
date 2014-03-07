8d16fdeab34bbf958f8b7c3ec065d7f42210a308
kardan <kardan@riseup.net>
2014-03-07 19:59:31 +0100

    polished
    Signed-off-by: kardan <kardan@riseup.net>

--- frn.pl
+++ frn.pl
@@ -7,6 +7,8 @@ use 5.12.1;
 use HTTP::Request::Common;
 use HTTP::Thin;
 use subs qw/debug help parse_options do_config check_config play_all play update_index fetch_all fetch save parse noentry/;
+
+## vars
 my %config = (url => "http://freie-radios.net", ua => LWP::UserAgent->new);
 $config{ua}->timeout(60); # failed try to reduce LWP CPU consumption.
 $config{ua}->add_handler( response_header => sub { 
@@ -18,9 +20,6 @@ $config{ua}->add_handler( response_header => sub {
 $|=1;
 
 ## subs
-sub debug {
-  print shift if ($config{debug});
-}
 sub help {
   print "This script downloads index and files from freie-radios.net, german political audio news. If called without arguments it only downloads index + info pages.
 \t-a -all mp3\tload all mp3 files
@@ -30,6 +29,7 @@ sub help {
 \t-h -help help\tshow this help\n";
   exit;
 }
+sub debug { print shift if ($config{debug}); }
 sub parse_options {
   foreach (@_) { # if called without arguments we only download index + info pages
     # TODO implement parsing for -and to verbosely download audio files and immedidately play them
@@ -51,70 +51,71 @@ sub do_config {
       if (mkdir $datadir) { $config{'datadir'} = $datadir; }
       else { warn "Could not create '$datadir': $!\n"; }
     }
-  } until ($config{datadir});
-  open my $sconfig, '>', "$config{dir}/config" or die "Could not save '$config{dir}/config': $!\n";
-  print $sconfig "datadir=$config{datadir}\n";
-  close $sconfig;
+  } until ($config{'datadir'});
+  open my $fh, '>', "$config{dir}/config" or die "Could not save '$config{dir}/config': $!\n";
+  print $fh "datadir=$config{datadir}\n";
+  close $fh;
+  return 1;
 }
 sub check_config {
   $config{'dir'} ||= "$ENV{'HOME'}/.frn";
-  $config{'index'} ||= 'index.html'; # TODO better publish this option
-  mkdir $config{'dir'} unless (-d $config{'dir'});
-  chdir "$config{dir}" || die "chdir $config{dir}: $!\n";
-  &do_config unless (-f 'config');
-  open my $lconfig, '<', "$config{dir}/config" or die "Could not open '$config{dir}/config': $!\n";
-  foreach (<$lconfig>) {
+  $config{'index'} ||= 'index.html'; # TODO publish this option in the config/manual
+  mkdir $config{'dir'} unless (-d $config{'dir'}); # create config dir
+  chdir "$config{dir}" || die "chdir $config{dir}: $!\n"; # access it or die
+  &do_config or die "Could not create config.\n" unless (-f 'config'); # create new config if necessary
+  open my $fh, '<', "$config{dir}/config" or die "Could not open '$config{dir}/config': $!\n";
+  foreach (<$fh>) { # read config
     if (/^(.+)=(.+)$/) {
       $config{$1} = $2;
     }
-  } close $lconfig;
-  mkdir $config{datadir} unless (-d $config{datadir});
+  } close $fh;
+  # prepare datadir
+  mkdir $config{datadir} unless (-d $config{datadir}); 
   chdir $config{datadir} or die "Could not access '$config{datadir}': $!\n";
   mkdir "html" unless (-d 'html');
   mkdir "mp3" unless (-d 'mp3');
+  return 1;
 }
 sub play_all {
   # TODO search & grep
-  if ($config{news}) {
-    my @list = qx{ls --sort time mp3/*.mp3};
+  if ($config{news}) { # start with youngest
+    my @list = qx{ls -1 --sort time mp3/*.mp3 };
     exit unless (@list >0);
     map { chomp; play($_) if (/\.mp3$/); } @list;
   } else {
-    my $list = join " ", glob('mp3/*');
-    system "mplayer $list" if ($list);
+    map { play($_) } glob('mp3/*');
   }
   exit;
 }
 sub play {
-  my $mp3 = shift || die "play(): no file given.\n";
+  my $mp3 = shift || warn "play(): no file given.\n" and return;
   print "\rPlaying $mp3.. ";
   system "mplayer -really-quiet $mp3 2>/dev/null";
-  sleep 1; # give the user a chance the CTRL+C the main process between mplayer calls
+  sleep 1; # give the user a chance to CTRL+C or read the filename if mplayer has issues
 }
 sub update_index {
-  if (1) { #unless (-f $config{index}) { # TODO check only if age > 1d?
+  #unless (-f $config{index}) { # TODO check only if age > 1d?
     print "Fetching index..";
     save(fetch($config{url}), 'index.html');
-  }
+  #}
   # load index.html
-  open my $lindex, '<', $config{'index'} or warn "Could not load '$config{'index'}': $!\n" and return;
-  foreach (<$lindex>) {
+  open my $fh, '<', $config{'index'} or warn "Could not load '$config{'index'}': $!\n" and return;
+  foreach (<$fh>) {
     if (/class="btitel"><a href="\/(\d+)">/) {
       print "\rLast entry: $1\n";
-      close $lindex;
+      close $fh;
       $config{'last'} = $1;
       return 1;
     }
-  } close $lindex;
+  } close $fh;
 }
 sub fetch {
   my $url = shift||die "fetch(): no url supplied.\n";
   my ($err, $res);
   do {
-    debug "\rLoading $url.. ";
+    debug "\rLoading $url.. [LWP] ";
     my $req = HTTP::Request->new(GET => $url);
-    debug "[LWP] ";
-    $res = $config{ua}->request($req);
+    $res = $config{ua}->request($req); # will output size and cancel (see above)
     debug "[Thin] ";
     $res = HTTP::Thin->new()->request(GET $url);
     unless ($res->is_success) { $err++;
@@ -122,9 +123,8 @@ sub fetch {
       print "[$err] $msg ";
       if ($msg =~ /^599/) { print $res->content; }
       sleep 10;
-    }
-    else { debug "done.\n"; }
-    sleep 1;
+    } else { debug "done.\n"; }
+    sleep 1; # give user a chance to cancel when interface disappeared etc.
   } until ($res->is_success || $err >=5);
   if ($err >=5) { print "Giving up for '$url'.\n"; }
   $res;
@@ -151,8 +151,9 @@ sub parse {
       push(@urls,$url);
     } elsif (/<td><h2 class="btitel_restricted">([^<]+)<\/h2><\/td>/) {
       print "$1\n";
+      # TODO add to db
     } elsif (/<td colspan="2">([^<]+)<\/td>/) {
-      # TODO save something
+      # TODO add to db
     }
   } close $fh;
   return @urls;
@@ -162,10 +163,10 @@ sub noentry {
   # if this file is deleted, all cached html are reparsed on the next run
   # if also the html cache has been deleted, we need to redownload all index files again
   # TODO create db file with titles and mp3 urls
-  my $id = shift||warn "failed(): no id given\n" and return;
-  open my $noentry, '>> noentry' or die "Could not write to 'noentry': $!\n";
-  print $noentry "$id\n";
-  close $noentry;
+  my $id = shift or warn "failed(): no id given\n" and return;
+  open my $fh, '>> noentry' or die "Could not write to 'noentry': $!\n";
+  print $fh "$id\n";
+  close $fh;
   return 1;
 }
 sub fetch_all { # retrieve html + mp3
@@ -175,30 +176,26 @@ sub fetch_all { # retrieve html + mp3
     map { $config{noentry}{$1}++ if (/(\d+)/) } <$missing>;
     close $missing;
   } else { warn "Could not open 'failed': $!\n"; }
+  # start downloads
   while ($entry >0) {
-    debug "\r$entry\r";
-    if ($config{noentry}{$entry}) { $entry--; next; }
-    printf "\r%80s\r", '';
-    print "\r$entry.html ";
+    if ($config{noentry}{$entry}) { $entry--; next; } # is handy if html cache got lost
+    printf "\r%80s\r$entry.html ", '';
     my $htmlfile = "html/$entry.html";
-    unless (-f $htmlfile) {
-      save(fetch("$config{url}/$entry") , $htmlfile);
-    }
+    save(fetch("$config{url}/$entry") , $htmlfile) unless (-f $htmlfile);
     if ($config{mp3}) { # should we download mp3 files also?
       foreach my $url (parse($htmlfile)) {
         chomp(my $file = qx{basename $url});
         # TODO check if the file is only partially downloaded
         unless (-f "mp3/$file") {
           print "$url > $file ";
-          save(fetch($url), "mp3/$file") && print "\n";
+          save(fetch($url), "mp3/$file");
+          print "\n";
           sleep 1;
         }
       }
-    }
-    $entry--;
-  }
-  print "\rSeemes as we downloaded all entries.\n";
-  return 1;
+    } $entry--;
+  } print "\rSeemes as we downloaded all entries.\n";
+  return 1; # TODO evaluate return value
 }
 #end subs
 
